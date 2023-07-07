062109d6d3cf7d4cc40662dc0f58b2d0b711ccc9
khairul20 <128460866+khairul20@users.noreply.github.com>
2023-07-07 20:46:26 +0700

    first commit

---
+++ app/Models/Pelanggan.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Pelanggan extends Model
+{
+    use HasFactory;
+    protected $table = "tb_pelanggan";
+    protected $primaryKey = "pel_id";
+    protected $guarded = [];
+
+
+    public function golongan(): BelongsTo
+    {
+        return $this->belongsTo(Golongan::class, 'pel_id_gol');
+    }
+}
